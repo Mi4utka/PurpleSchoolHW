09935669ca750bf09dfef66c0d29a52ce2867b8c
mi4utla <mix.maisaa@mail.ru>
2024-03-19 10:19:58 +0300

    3d homework done

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const makeMeUniquePlease = 
-[
-    {id : 1, name : 'Вася'},
-    {id : 2, name : 'Вася'},
-    {id : 1, name : 'Вася'}
-]
-
- const almostThere = new Set(makeMeUniquePlease.map((obj) => obj.id))
- const res = []
- for(const id of almostThere){
-    res.push(makeMeUniquePlease.find((elem) => {
-if(elem.id === id){
-    return elem.name
+const areYou = (birhdayDate) => {
+    const birthday = new Date(birhdayDate)
+const now = new Date()
+const almost = now - birthday
+const fourteen = new Date(1984, 0, 1)
+if(almost < fourteen){
+    return false
 }
-    }))}
- 
- console.log(res)
+return true
+}
+const date1 = '2022-01-11'
+const date2 = '1999-03-14'
+console.log(areYou(date1))
+console.log(areYou(date2))
