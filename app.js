7a6ed7d6e5ea242270b4cf4b8a8b83f6b688065e
mi4utla <mix.maisaa@mail.ru>
2024-03-20 17:50:36 +0300

    hw 5 done

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-const uni = (arr) => {
-  const uniqueArray = [];
-  const idsSet = new Set();
-
-  arr.forEach((obj) => {
-    if (!idsSet.has(obj.id)) {
-      idsSet.add(obj.id);
-      uniqueArray.push(obj);
-    }
-  });
-
-  return uniqueArray;
+const Hero = function (race, name, language) {
+  this.race = race;
+  this.name = name;
+  this.language = language;
 };
-const makeMeUniquePlease = [
-  { id: 1, name: 'Вася' },
-  { id: 2, name: 'Вася' },
-  { id: 1, name: 'Вася' },
-];
-console.log(uni(makeMeUniquePlease));
+Hero.prototype.speak = function () {
+  console.log(this.language);
+  console.log(this.name);
+};
+
+const Ork = function (race, name, language, weapon) {
+  Hero.call(this, race, name, language);
+  this.weapon = weapon;
+};
+Ork.prototype = Object.create(Hero.prototype);
+Ork.prototype.constructor = Ork;
+
+Ork.prototype.attack = function () {
+  console.log('gg');
+};
+
+const Elf = function (race, name, language, spell) {
+  Hero.call(this, race, name, language);
+  this.spell = spell;
+};
+Elf.prototype = Object.create(Hero.prototype);
+Elf.prototype.constructor = Elf;
+
+Elf.prototype.createSpell = function () {
+  console.log('nt');
+};
+
+const elf = new Elf('none', 'john', 'eng', 'fireball');
+const ork = new Ork('none', 'Nikita', 'ru', 'sword');
+ork.attack();
+elf.createSpell();
